4c01d4c67666aca391622a4e204bd3470acf12fe
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-09-04 17:29:45 +0200

    Styling

--- src/App.js
+++ src/App.js
@@ -9,11 +9,16 @@ function App() {
   const [list, setList] = useState([]);
   const [update, setUpdate] = useState(false);
   const [updateID, setUpdateID] = useState(null);
-  const [info, setInfo] = useState({});
+  const [info, setInfo] = useState({show: false, msg: '', type: ''});
 
   return (
     <div className="App">
       <h1>Shopping list</h1>
+      <form></form>
+      <div className="list_container">
+        <List />
+        <button className='btn_clear'>Clear list</button>
+      </div>
     </div>
   );
 }
