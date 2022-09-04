cf770f9057075f424d35d26174827313152721a4
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-09-04 17:49:55 +0200

    Update main fn

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import List from "./Component/List/List";
 import Info from "./Component/Info/Info";
 
 function App() {
-  const [name, setName] = useState("");
+  const [item, setItem] = useState("");
   const [list, setList] = useState([]);
   const [update, setUpdate] = useState(false);
   const [updateID, setUpdateID] = useState(null);
@@ -13,6 +13,14 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    if(!item) {
+      // display info
+    } else if(item & update) {
+      // edit the item
+    } else {
+      // add item to list
+    }
   };
 
   return (
@@ -21,8 +29,15 @@ function App() {
       <form className="item_form" onSubmit={handleSubmit}>
         {info.show && <Info />}
         <div className="input_container">
-          <input type="text" />
-          <button type="submit" className="btn_submit">{update ? 'Edit' : 'Add'}</button>
+          <input
+            type="text"
+            placeholder="Add your item"
+            value={item}
+            onChange={(e) => setItem(e.target.value)}
+          />
+          <button type="submit" className="btn_submit">
+            {update ? "Edit" : "Add"}
+          </button>
         </div>
       </form>
       <div className="list_container">
