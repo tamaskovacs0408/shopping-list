9e289d9beab6abcd75d3a5c28e2ff99af50854e9
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-09-04 17:19:39 +0200

    Create useState hooks, install uuid

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
 import React, {useState, useEffect} from 'react';
+import uuid from 'react-uuid';
 import List from './Component/List/List';
 import Info from './Component/Info/Info';
 
 function App() {
+  const [name, setName] = useState('');
+  const [list, setList] = useState([]);
+  const [update, setUpdate] = useState(false);
+  const [updateID, setUpdateID] = useState(null);
+  const [info, setInfo] = useState({});
+
   return (
     <div className="App">
       <h1>Shopping list</h1>
