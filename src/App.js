def0477806f1e02784f299dc0b47f05cfce73052
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-09-04 17:15:11 +0200

    Main style settings

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import React, {useState, useEffect} from 'react';
+import List from './Component/List/List';
+import Info from './Component/Info/Info';
 
 function App() {
   return (
     <div className="App">
+      <h1>Shopping list</h1>
     </div>
   );
 }
