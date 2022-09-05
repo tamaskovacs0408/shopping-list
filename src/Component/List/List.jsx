df4bde820c9c60eba89cfe48791a6950f7be2b44
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-09-05 09:13:31 +0200

    Install fontawesome icons

--- src/Component/List/List.jsx
+++ src/Component/List/List.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { faFileEdit } from '@fortawesome/free-solid-svg-icons';
 
 export default function List({items}) {
   return (
@@ -7,6 +8,12 @@ export default function List({items}) {
         const {id, title} = item;
         return <div key={id} className='list_item'>
           <p className='item_title'>{title}</p>
+          <div className="btn_container">
+            <button className='btn_edit'>
+              <faFileEdit />
+            </button>
+            <button></button>
+          </div>
         </div>
       })}
     </div>
