1a53a1bc26e8d0f55378afe50d7c5226ecc6dd63
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-09-06 10:44:57 +0200

    Add animation to Info

--- src/Component/Info/Info.jsx
+++ src/Component/Info/Info.jsx
@@ -1,17 +1,18 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from "react";
 
-export default function Info({type, msg, removeInfo, list}) {
+export default function Info({ type, msg, removeInfo, list }) {
   useEffect(() => {
     const timeOut = setTimeout(() => {
-      removeInfo()
-    }, 1500)
-    return () => clearTimeout(timeOut)
-  }, [list]) // Once anything changes on the list, clear timeout and set up new one.
-  
+      removeInfo();
+    }, 2000);
+    return () => clearTimeout(timeOut);
+  }, [list]); // Once anything changes on the list, clear timeout and set up new one.
 
   return (
     <div className="info_container">
-      <p className={`info info_${type}`}>{msg}</p>
+      <p className={`info info_${type} animate__animated animate__fadeOutRight`}>
+        {msg}
+      </p>
     </div>
-  )
+  );
 }
