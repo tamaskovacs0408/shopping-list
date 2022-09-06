69740349ef59518b0cdcfbd91afbf1a890996748
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-09-06 11:39:57 +0200

    Update styles

--- src/Component/List/List.jsx
+++ src/Component/List/List.jsx
@@ -1,24 +1,29 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEdit, faXmark } from '@fortawesome/free-solid-svg-icons';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEdit, faXmark } from "@fortawesome/free-solid-svg-icons";
 
-export default function List({items, deleteItem, updateItem}) {
+export default function List({ items, deleteItem, updateItem }) {
   return (
-    <div className='shopping_list'>
+    <div className="shopping_list">
       {items.map((item) => {
-        const {id, title} = item;
-        return <div key={id} className='list_item'>
-          <p className='item_title'>{title}</p>
-          <div className="btn_container">
-            <button className='btn_edit'>
-              <FontAwesomeIcon icon={faEdit} onClick={() => updateItem(id)}/>
-            </button>
-            <button className='btn_delete' >
-              <FontAwesomeIcon icon={faXmark} onClick={() => deleteItem(id)}/>
-            </button>
+        const { id, title } = item;
+        return (
+          <div key={id} className="list_item">
+            <p className="item_title">{title}</p>
+            <div className="btn_container">
+              <button className="btn_edit">
+                <FontAwesomeIcon icon={faEdit} onClick={() => updateItem(id)} />
+              </button>
+              <button className="btn_delete">
+                <FontAwesomeIcon
+                  icon={faXmark}
+                  onClick={() => deleteItem(id)}
+                />
+              </button>
+            </div>
           </div>
-        </div>
+        );
       })}
     </div>
-  )
+  );
 }
